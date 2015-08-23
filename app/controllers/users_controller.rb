b03187ffdd4c68c57cc5b017309b39f5b7126721
Mary Burford <mburford@paperlesspost.net>
2015-08-23 15:40:39 -0400

    show random matches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def matches
-
+  	@random_users = User.joins(:profile).select('profiles.id, users.user_name, users.id, profiles.profile_photo').limit(5).order("RANDOM()")
   end
 
 
